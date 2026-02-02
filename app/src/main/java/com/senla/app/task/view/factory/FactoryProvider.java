0e27d5e1022479e6bfe7b715988962f9b87994ca
albert <albert.foreber@gmail.com>
2026-02-02 22:40:14 +0300

    fixes from task14

---
+++ app/src/main/java/com/senla/app/task/view/factory/FactoryProvider.java
@@ -0,0 +1,18 @@
+package com.senla.app.task.view.factory;
+
+import org.springframework.stereotype.Component;
+import java.util.Map;
+
+@Component
+public class FactoryProvider {
+
+    private final Map<String, MenuFactory> factories;
+
+    public FactoryProvider(Map<String, MenuFactory> factories) {
+        this.factories = factories;
+    }
+
+    public MenuFactory getFactory(String factoryName) {
+        return factories.get(factoryName);
+    }
+}
